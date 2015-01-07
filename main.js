13d47a5f2c19b7c962aad569a2f9042b3d473c88
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-06 21:11:04 -0500

    I have no idea what's happening

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-chrome.tabs.query({'active': true}, function(tabs) {
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
+debugger
+
+$(function(){
+  $("body").text("hi");
+  // Operates on popup.html
+  $("body").css({backgroundColor: "red"});
 });
+// chrome.tabs.query({'active': true}, function(tabs) {
+//   chrome.tabs.executeScript({
+//     code: '$("body").css({backgroundColor: "red"})'
+//   });
+// });
