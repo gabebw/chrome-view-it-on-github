00722217ba0021043436bf6ad7a7112458da11e5
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-09 11:41:41 -0500

    WIP: Try to get the delete button working
    Roadblocks:
    
    * `.trigger("click")` doesn't seem to work
    * Which element is the event listener attached to?
    * GMail doesn't even create the delete button (so it's not findable by jQuery)
      until you mouse over it.

--- main.js
+++ main.js
@@ -8,18 +8,31 @@ function clearBody(){
   var githubLink = jQuery("a:contains('view it on GitHub'):last").eq(0);
   var githubURL = githubLink.attr("href");
 
-  deleteButton.trigger("mousedown");
-  deleteButton.trigger("mouseup");
-  deleteButton.trigger("keydown");
-  deleteButton.trigger("keyup");
-  deleteButton.trigger("click");
+  console.log(deleteButton);
+  console.log(deleteButton.length);
+  console.log(deleteButton.attr("class"));
+
+  $(document.body).trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().parent().parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().parent().parent().parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().parent().parent().parent().parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+  deleteButton.parent().parent().parent().parent().parent().parent().parent().parent().trigger({ type: "keydown", which: "#".charCodeAt(0) });
+
+
   // chrome.runtime.sendMessage({"message": "open_github_page", "url": githubURL});
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "click_github_link" ) {
-      clearBody();
+window.addEventListener("load", function(event){
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "click_github_link" ) {
+        clearBody();
+      }
     }
-  }
-);
+  );
+});
