76ea34b12dd6859f8f1afe5637557a2a3541e7d5
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-08 23:11:15 -0500

    Chrome tabs are successfully receiving messages
    * jQuery is working
    * Send a meaningful message to the active tab

--- background.js
+++ background.js
@@ -6,8 +6,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "hello"});
-    console.log("SENT MESSAGE TO ", activeTab.url, "("+activeTab.status+")");
+    chrome.tabs.sendMessage(activeTab.id, {"message": "click_github_link"});
   });
 });
 
--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-function _overlay() {
-  var text = document.createTextNode("HELLO THERE");
-  var o = document.createElement("div");
-  o.setAttribute("id", "instapaper_overlay");
-  o.appendChild(text);
-  // o.setAttribute('id', 'ov');
-  // o.setAttribute('class', 'instapaper_overlay instapaper_ignore');
-  return o;
-}
-
 function clearBody(){
-  var body = document.createElement("body");
-  document.body.appendChild(_overlay());
+  // $ doesn't work but jQuery does, for some reason
+  jQuery("body").html("");
+  // This is the actual console that you can see
+  console.log("HELLO");
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    clearBody();
-    $("body").html("");
+    if( request.message === "click_github_link" ) {
+      clearBody();
+    }
   }
 )
