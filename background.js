68021e7d30719371af8e37e375f3956355cdd282
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-08 23:04:21 -0500

    It's kind of working

---
+++ background.js
@@ -0,0 +1,17 @@
+// This script runs in the background.
+console.log("hi from background.js");
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "hello"});
+    console.log("SENT MESSAGE TO ", activeTab.url, "("+activeTab.status+")");
+  });
+});
+
+// Listens to messages sent by chrome.tabs.sendMessage
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log("LISTENER FIRED");
+});
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-debugger
+function _overlay() {
+  var text = document.createTextNode("HELLO THERE");
+  var o = document.createElement("div");
+  o.setAttribute("id", "instapaper_overlay");
+  o.appendChild(text);
+  // o.setAttribute('id', 'ov');
+  // o.setAttribute('class', 'instapaper_overlay instapaper_ignore');
+  return o;
+}
 
-$(function(){
-  $("body").text("hi");
-  // Operates on popup.html
-  $("body").css({backgroundColor: "red"});
-});
-// chrome.tabs.query({'active': true}, function(tabs) {
-//   chrome.tabs.executeScript({
-//     code: '$("body").css({backgroundColor: "red"})'
-//   });
-// });
+function clearBody(){
+  var body = document.createElement("body");
+  document.body.appendChild(_overlay());
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    clearBody();
+    $("body").html("");
+  }
+)
