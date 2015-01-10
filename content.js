97139186cae1ea79cd51e415d593a89ad782a5e0
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-09 20:24:29 -0500

    Actually open the GitHub URL in a new page
    Use a function name that reflects its purpose.

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
-function clearBody(){
+function openGithubLinkInNewTab(){
   var githubLink = jQuery("a:contains('view it on GitHub'):last").eq(0);
   var githubURL = githubLink.attr("href");
 
-  // chrome.runtime.sendMessage({"message": "open_github_page", "url": githubURL});
+  chrome.runtime.sendMessage({"message": "open_github_page", "url": githubURL});
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "click_github_link" ) {
-      clearBody();
+      openGithubLinkInNewTab()
     }
   }
 );
