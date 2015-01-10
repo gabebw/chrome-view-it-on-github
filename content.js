1d851c73f56528ff6bc9ba7821962147f4f31f63
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-09 20:24:23 -0500

    Remove delete-button-related code

--- content.js
+++ content.js
@@ -1,13 +1,7 @@
 function clearBody(){
   var githubLink = jQuery("a:contains('view it on GitHub'):last").eq(0);
   var githubURL = githubLink.attr("href");
-  var deleteButton = jQuery("[data-tooltip='Delete']:visible").eq(0);
 
-  deleteButton.trigger("mousedown");
-  deleteButton.trigger("mouseup");
-  deleteButton.trigger("keydown");
-  deleteButton.trigger("keyup");
-  deleteButton.trigger("click");
   // chrome.runtime.sendMessage({"message": "open_github_page", "url": githubURL});
 }
 
